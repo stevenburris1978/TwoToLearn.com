06f5aa251b9566d9c249292f4d4e299d7461b96f
stevenburris1978 <s_burris@mail.fhsu.edu>
2023-05-15 11:05:53 -0500

    first commit

--- src/components/Lesson/lessoncart/LessonCart.jsx
+++ src/components/Lesson/lessoncart/LessonCart.jsx
@@ -3,38 +3,43 @@ import "./lessoncart.css";
 import Card from "../../shared/card/Card";
 import Lesson from "../lesson/Lesson";
 
-const LessonCart = () => {
-  const { cartLessons, calculateTotalPrice } =
-    Cart();
+// const LessonCart = () => {
+//   const { cartLessons, calculateTotalPrice } =
+//     Cart();
 
 
     //* {cartLessons.length > 0 ? ( //*
+
+    export default function LessonCart() {
+    
   return (
     <Card className="container">
-      {cartLessons.length ? (
-        <div>
-          {cartLessons.map((lesson) => (
-          <Lesson
-            key={lesson.id}
-            id={lesson.id}
-            category={lesson.category}
-            price={lesson.price}
-            lesson={lesson}
-          />
-          ))}
 
-          <div className="bottom">
 
-              <h3 className="total">Total: ${calculateTotalPrice()}</h3>
+    {/* //   {cartLessons.length ? (
+    //     <div>
+    //       {cartLessons.map((lesson) => (
+    //       <Lesson
+    //         key={lesson.id}
+    //         id={lesson.id}
+    //         category={lesson.category}
+    //         price={lesson.price}
+    //         lesson={lesson}
+    //       />
+    //       ))}
 
-          </div>
+    //       <div className="bottom">
 
-        </div>
-      ) : (
-        <h2 className="empty">Your cart is empty!</h2>
-      )}
+    //           <h3 className="total">Total: ${calculateTotalPrice()}</h3>
+
+    //       </div>
+
+    //     </div>
+    //   ) : (
+    //     <h2 className="empty">Your cart is empty!</h2>
+    //   )} */}
+
     </Card>
   );
-};
+}
 
-export default LessonCart;
